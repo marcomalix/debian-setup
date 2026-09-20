ed14eee89c0a49337928004e4e5311d952bc54d4
malik <88252252+marcomalix@users.noreply.github.com>
2026-09-20 21:34:05 +0100

    Add duf to the packages

--- debian-postinstall-script.sh
+++ debian-postinstall-script.sh
@@ -184,7 +184,7 @@ fi
 if [[ "$DO_CORE_APPS" == true ]]; then
     log "Installing core applications."
     apt_install alacritty evince rofi plank qalculate-gtk
-    apt_install htop btop fastfetch
+    apt_install htop btop fastfetch duf
     apt_install geany geany-plugin-addons geany-plugin-git-changebar \
         geany-plugin-overview geany-plugin-spellcheck geany-plugin-treebrowser \
         geany-plugin-markdown
